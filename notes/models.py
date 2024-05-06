2c0e6571bf01050a09f403dfc0c8bfdc78409fb1
tettehnarh <leslienarh@outlook.com>
2024-05-06 10:36:11 +0000

    created notes model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Notes'
